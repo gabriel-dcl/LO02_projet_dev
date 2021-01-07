8b8596f80b982b57ddd061e28ae90dad387219ab
Drake <Drake@MSI>
2021-01-07 22:09:26 +0100

    Ajout du Board dans la vue graphique

--- src/Vues/graphiques/GameGraphicVue.java
+++ src/Vues/graphiques/GameGraphicVue.java
@@ -5,6 +5,7 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.SpringLayout.Constraints;
 
 import java.awt.GridBagLayout;
 
@@ -14,9 +15,12 @@ import java.awt.GridBagConstraints;
 import java.awt.Insets;
 
 import Controllers.GameController;
+import Models.Card;
+import Models.Coordinate;
 import Vues.Vue;
 
 import java.awt.event.ActionListener;
+import java.util.Map;
 import java.util.Observable;
 import java.util.Observer;
 import java.awt.event.ActionEvent;
@@ -28,7 +32,13 @@ public class GameGraphicVue implements Vue, Observer, Runnable {
 	private GameController ctrl;
 	private boolean playerTurn = false;
 	private boolean gameOver = false;
-	
+	private JButton Board[][] = new JButton[12][12];
+	private JButton btnShowVCard;
+	private JButton btnPlaceNewCard;
+	private JButton btnMoveCard;
+	private JButton btnShuffle;
+	private JButton btnAlternateCards;
+	private JButton btnEndTurn;
 	protected ImageIcon createImageIcon(String path, String description) {
 		
 			if (path != null) {
@@ -90,21 +100,8 @@ public class GameGraphicVue implements Vue, Observer, Runnable {
 				0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
 		frame.getContentPane().setLayout(gridBagLayout);
 
-		JButton btnShowVCard = new JButton("Afficher Carte Victoire");
-		btnShowVCard.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				JButton label1 = new JButton(createImageIcon("Cartes/EBC.jpg", "Un cercle"));
-				GridBagConstraints gbc_label1 = new GridBagConstraints();
-				gbc_label1.insets = new Insets(0, 0, 5, 5);
-				gbc_label1.gridx = 1;
-				gbc_label1.gridy = 14;
-				frame.getContentPane().add(label1, gbc_label1);
-				frame.setVisible(false);
-				frame.setVisible(true);
-				label1.setIcon(createImageIcon("Cartes/EBC.png", "Un cercle"));
-				label1.setBackground(Color.LIGHT_GRAY);
-			}
-		});
+		btnShowVCard = new JButton("Afficher Carte Victoire");
+		
 		
 		
 		
@@ -114,66 +111,49 @@ public class GameGraphicVue implements Vue, Observer, Runnable {
 		gbc_btnShowVCard.gridy = 2;
 		frame.getContentPane().add(btnShowVCard, gbc_btnShowVCard);
 
-		JButton btnPlaceNewCard = new JButton("Placer la nouvelle carte");
-		btnPlaceNewCard.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				ctrl.addCardOnBoard(null, null);
-			}
-		});
+		btnPlaceNewCard = new JButton("Placer la nouvelle carte");
+		
 		GridBagConstraints gbc_btnPlaceNewCard = new GridBagConstraints();
 		gbc_btnPlaceNewCard.insets = new Insets(0, 0, 5, 5);
 		gbc_btnPlaceNewCard.gridx = 1;
 		gbc_btnPlaceNewCard.gridy = 4;
 		frame.getContentPane().add(btnPlaceNewCard, gbc_btnPlaceNewCard);
 
-		JButton btnMoveCard = new JButton("D\u00E9placer une carte");
-		btnMoveCard.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				
-			}
-		});
+		btnMoveCard = new JButton("D\u00E9placer une carte");
 		GridBagConstraints gbc_btnMoveCard = new GridBagConstraints();
 		gbc_btnMoveCard.insets = new Insets(0, 0, 5, 5);
 		gbc_btnMoveCard.gridx = 1;
 		gbc_btnMoveCard.gridy = 6;
 		frame.getContentPane().add(btnMoveCard, gbc_btnMoveCard);
 
-		JButton btnShuffle = new JButton("Shuffle !");
-		btnShuffle.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				
-			}
-		});
+		btnShuffle = new JButton("Shuffle !");
+		
 		GridBagConstraints gbc_btnShuffle = new GridBagConstraints();
 		gbc_btnShuffle.insets = new Insets(0, 0, 5, 5);
 		gbc_btnShuffle.gridx = 1;
 		gbc_btnShuffle.gridy = 8;
 		frame.getContentPane().add(btnShuffle, gbc_btnShuffle);
 
-		JButton btnAlternateCards = new JButton("Echanger 2 cartes");
-		btnAlternateCards.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				ctrl.alternateCards(null, null);
-			}
-		});
+		btnAlternateCards = new JButton("Echanger 2 cartes");
+		
 		GridBagConstraints gbc_btnAlternateCards = new GridBagConstraints();
 		gbc_btnAlternateCards.insets = new Insets(0, 0, 5, 5);
 		gbc_btnAlternateCards.gridx = 1;
 		gbc_btnAlternateCards.gridy = 10;
 		frame.getContentPane().add(btnAlternateCards, gbc_btnAlternateCards);
 
-		JButton btnNewButton_5 = new JButton("Fin du tour");
-		btnNewButton_5.setBackground(UIManager.getColor("Button.background"));
-		btnNewButton_5.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				
-			}
-		});
-		GridBagConstraints gbc_btnNewButton_5 = new GridBagConstraints();
-		gbc_btnNewButton_5.insets = new Insets(0, 0, 5, 5);
-		gbc_btnNewButton_5.gridx = 1;
-		gbc_btnNewButton_5.gridy = 12;
-		frame.getContentPane().add(btnNewButton_5, gbc_btnNewButton_5);
+		btnEndTurn = new JButton("Fin du tour");
+		btnEndTurn.setBackground(UIManager.getColor("Button.background"));
+		
+		GridBagConstraints gbc_btnEndTurn = new GridBagConstraints();
+		gbc_btnEndTurn.insets = new Insets(0, 0, 5, 5);
+		gbc_btnEndTurn.gridx = 1;
+		gbc_btnEndTurn.gridy = 12;
+		frame.getContentPane().add(btnEndTurn, gbc_btnEndTurn);
+		
+		drawBoard();
+		
+		
 	}
 	@Override
 	public void run() {
@@ -199,10 +179,91 @@ public class GameGraphicVue implements Vue, Observer, Runnable {
             // handle if you like
         }
 	}
+	
 	private void playerTurn() {
-		// TODO Auto-generated method stub
+		btnShowVCard.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+			}
+		});
 		
+		btnPlaceNewCard.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				ctrl.addCardOnBoard(null, null);
+			}
+		});
+		
+		btnMoveCard.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+			}
+		});
+		
+		
+		btnShuffle.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+			}
+		});
+		
+		btnAlternateCards.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				ctrl.alternateCards(null, null);
+			}
+		});
+		
+		btnEndTurn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+			}
+		});
 	}
+	
+	public void drawBoard()
+    {
+        for (int i = 0; i < 12; i++)
+        {
+            for (int j = 0; j < 12; j++)
+            {
+            	Board[i][j] = new JButton();
+				GridBagConstraints gbc_label1 = new GridBagConstraints();
+				gbc_label1.insets = new Insets(0, 0, 5, 5);
+				gbc_label1.gridx = 3+i;
+				gbc_label1.gridy = 2+j;
+				frame.getContentPane().add(Board[i][j], gbc_label1);
+				
+				Board[i][j].setIcon(createImageIcon("Cartes/Blank.png", "Vide"));
+				Board[i][j].setBackground(Color.LIGHT_GRAY);
+            }
+            
+        }
+        frame.setVisible(false);
+		frame.setVisible(true);
+    }
+	
+	public void showBoard()
+    {
+        for (int i = 0; i < 12; i++)
+        {
+            for (int j = 0; j < 12; j++)
+            {
+                boolean hasFound = false;
+
+                for (Map.Entry<Coordinate, Card> entry : ctrl.getGameManager().getCurrentBoard().getCurrentCardsOnBoard().entrySet())
+                {
+                    if (entry.getKey().equals( new Coordinate(i, j)) )
+                    {
+                        
+                        String nomCarte = "Cartes/"+ctrl.getGameManager().getCurrentBoard().getCardByCoordinate(entry.getKey()).toStringGraphic()+".png";
+                        Board[i][j].setIcon(createImageIcon(nomCarte, "Vide"));
+                        hasFound = true;
+                    }
+                }
+                if(!hasFound)
+                	Board[i][j].setIcon(createImageIcon("Cartes/Blank.png", "Vide"));
+            }
+        }
+    }
 	
 	@Override
 	public void update(Observable arg0, Object arg1) {
